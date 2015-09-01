fe05d1a3d4f0ffc41e3d4b4e08c24929e5ee0564
Tomas Cordero <tcorderoiv@gmail.com>
2015-09-01 00:03:37 -0700

    Finally got the phpFlickr working

--- gallery/apitest.php
+++ gallery/apitest.php
@@ -1,19 +1,7 @@
 <?php
-require_once('../assets/api/flickr.php');
+require_once('../assets/api/phpflickr/phpFlickr.php');
 
-$flickr = new FlickrPHP("XXX", "133398630@N03");
-$photosReturn = $flickr->getAlbums();
-// $photosetID = $photosReturn->photosets->photoset[3]->id;
+$f = new phpFlickr("######", "######", false);
+$recent = $f->photos_getSizes("20321869573");
 
-// $photo = $flickr->getPhotos($photosetID);
-
-// // $photosizes = $flickr->getSizes($photo->photoset->primary);
-// foreach ($photo->photoset->photo as $single) {
-// 	$photosizes = $flickr->getSizes($single->id);
-// 	echo "<img src='".$photosizes->sizes->size[3]->source."' />";
-// }
-$contents = file('album-list.json');
-echo "<pre>";
-var_dump($contents);
-// var_dump($photosReturn->photosets->photoset[0]);
-echo "</pre>";
+var_dump($recent);
